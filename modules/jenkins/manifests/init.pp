b1d3fe83c384d13a4b1d2360ef79e6ea0807ba27
Kris Buytaert <sdog@inuits.be>
2011-08-12 12:22:26 +0200

    Adding plenty of plugins and the inuits plugin repo (rpm only for now)

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -48,7 +48,52 @@ class jenkins{
 	include jenkins::package
 	Class["jenkins::repo"] -> Class["jenkins::package"]
 	include jenkins::enabled 
+	include jenkins::plugins 
 
 }
 
+
+class jenkins::plugins{
+
+	  yumrepo { "inuits-jenkins-plugins":
+            descr       => "inuits-jenkins-plugins",
+            enabled     => 1,
+            gpgcheck    => 0,
+            baseurl     => "http://repo.inuits.be/jenkins/os";
+
+        }
+
+
+	package {
+		"jenkins-analysis-core-plugin":
+		     ensure => present;
+		"jenkins-checkstyle-plugin":
+		     ensure => present;
+		"jenkins-clover-plugin":
+		     ensure => present;
+		"jenkins-doxygen-plugin":
+		     ensure => present;
+		"jenkins-dry-plugin":
+		     ensure => present;
+		"jenkins-git-plugin":
+		     ensure => present;
+		"jenkins-htmlpublisher-plugin":
+		     ensure => present;
+		"jenkins-jdepend-plugin":
+		     ensure => present;
+		"jenkins-plot-plugin":
+		     ensure => present;
+		"jenkins-pmd-plugin":
+		     ensure => present;
+		"jenkins-promoted-builds-plugin":
+		     ensure => present;
+		"jenkins-publish-over-ssh-plugin":
+		     ensure => present;
+		"jenkins-violations-plugin":
+		     ensure => present;
+		"jenkins-xunit-plugin":
+		     ensure => present;
+
+	}
+}	
 
